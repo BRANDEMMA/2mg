693fd15769b098598c6b666d09ca2cc4505759c3
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-05-16 12:50:48 -0500

    Añadí ubicación en Lima

--- pie-pagina.php
+++ pie-pagina.php
@@ -22,7 +22,7 @@
 				</div>
 			</div>
 			<div class="derecha-mapas">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15605.95413749387!2d-77.0051997!3d-12.07867125!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2spe!4v1462288585293" width="600" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2758.142565195794!2d-77.00351110031148!3d-12.139318669342536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbb6181ac292e7fb7!2s2MG+Ingenieria%2C+Comercio+y+Representaciones+SAC!5e0!3m2!1ses!2sus!4v1463420647804" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
 
 			<div class="clear"></div>
