77cbc05a905f5a7e3008874a8a990cb62ec66b23
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-07-20 13:34:44 -0500

    agregué Google Analytics

--- ingles/index.php
+++ ingles/index.php
@@ -27,6 +27,21 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 	<!--================== 	ENLACE ICONO (TERMINA)  =====================-->
+
+	<!--================== 	ENLACE GOOGLE ANALYTICS (INICIA)  =====================-->
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-79840828-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
+	<!--================== 	ENLACE GOOGLE ANALYTICS (TERMINA)  =====================-->
 </head>
 
 <body>
