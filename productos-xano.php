33ca00b921f21e0d0b5ffaf8f7108b2a6ac9c169
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-08-10 12:39:45 -0500

    Productos xado

---
+++ productos-xano.php
@@ -0,0 +1,164 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<html lang="es">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width" />
+	<title>2MG - SOMOS EXPORTADORES E IMPORTADORES DE PRODUCTOS, DAMOS SERVICIO Y SOLUCION EN PROCUREMENT DE INGENIERÍA.</title>
+	<meta name="author" content="Brandemma" />
+	<meta name="description" content="2MG es una empresa peruana que se dedica a la importación y exportación de productos, servicios y soluciones en Procurement de Ingeniería."/>
+
+	<!--================== 	ENLACE HOJA DE ESTILOS (INICIA)  =====================-->
+
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+	<!--================== 	ENLACE HOJA DE ESTILOS (TERMINA)  =====================-->
+
+	<!--================== 	ENLACE ICONO (INICIA)  =====================-->
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+	<!--================== 	ENLACE ICONO (TERMINA)  =====================-->
+
+	<!--================== 	ENLACE GOOGLE ANALYTICS (INICIA)  =====================-->
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-79840828-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
+	<!--================== 	ENLACE GOOGLE ANALYTICS (TERMINA)  =====================-->
+
+</head>
+
+<body>
+	<header>
+		<?php
+			include 'menu.php';
+		?>
+	</header>
+	
+	<div class="contenedor-xado">
+		
+		<img class="banner-xado" src="img/banner-xado.jpg">
+
+		<div class="descripcion-xado">
+			<div class="texto-xado-izq">
+				<img src="img/marco-xado.jpg">
+			</div>
+			
+			<div class="texto-xado-der">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin commodo aliquet velit, ut elementum ante auctor vel. Nam pulvinar porta pellentesque. Pellentesque molestie tortor quis elementum rhoncus. Nam molestie massa enim, sit amet consectetur nisl viverra quis. Nulla nec neque eu est dignissim ornare vel quis ante. Sed ac mauris ac ante eleifend ultricies eget non eros. Aenean commodo vehicula est, sed gravida velit. Curabitur id tincidunt orci.<br><br>
+
+				Sed quis metus mi. Integer condimentum neque pharetra tellus consequat ultrices. Donec efficitur eu diam a ornare. Nullam id mauris accumsan, consequat ligula eget, lacinia eros. In nec mattis nunc. Integer commodo rhoncus felis in varius. Duis id felis lobortis, rhoncus lectus laoreet, blandit ante. Nullam in imperdiet nulla, nec vestibulum odio. Integer et erat velit. Duis sem neque, dictum quis urna quis, efficitur auctor enim. Morbi scelerisque arcu eget metus molestie fermentum. Quisque ornare, nibh et malesuada aliquam, nulla mi blandit magna, sed viverra diam ante id nunc.</p>
+
+				<a class="boton-azul-xado" href="">Leer Más</a>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+
+			<h1 class="titulo-xado">Productos XADO</h1>
+
+			<div class="subtitulo-xado">
+				<h2>Productos &nbsp; <i class="fa fa-sort-desc" aria-hidden="true"></i></h2>
+				<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+			</div>
+
+			<div class="contenedor-productos-xado">
+
+				<div class="producto-xado">
+					<img src="img/producto-xado1.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+				<div class="producto-xado">
+					<img src="img/producto-xado2.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+				<div class="producto-xado">
+					<img src="img/producto-xado3.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+				<div class="producto-xado">
+					<img src="img/producto-xado4.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+				<div class="producto-xado">
+					<img src="img/producto-xado4.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+				<div class="producto-xado">
+					<img src="img/producto-xado3.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+				<div class="producto-xado">
+					<img src="img/producto-xado2.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+				<div class="producto-xado">
+					<img src="img/producto-xado1.jpg">
+					<h3>XADO</h3>
+					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<p class="codigo-producto-xado">CODIGO: 97590</p>
+
+					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
+				</div>
+
+
+			</div>
+
+		</div>
+
+		<?php
+			include 'contacto.php';
+		?>
+	</div>
+
+	<?php
+		include 'pie-pagina.php';
+	?>
+</body>
+
+</html>
