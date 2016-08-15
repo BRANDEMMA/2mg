500792de31fae50cf559cf98c24da6ceede8c160
rockerny <eegoavil@brandemma.com>
2016-08-15 12:59:54 -0500

    Cambios en la alineación

--- productos-xado.php
+++ productos-xado.php
@@ -6,36 +6,36 @@
 	<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width" />
-	<title>2MG - XADO.</title>
+	<title>2MG - XADO</title>
 	<meta name="author" content="Brandemma" />
 	<meta name="description" content="2MG es una empresa peruana que se dedica a la importación y exportación de productos, servicios y soluciones en Procurement de Ingeniería."/>
 
-	<!--================== 	ENLACE HOJA DE ESTILOS (INICIA)  =====================-->
+	<!--================== 	ENLACE HOJA DE ESTILOS <br>(INICIA)  =====================-->
 
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 
-	<!--================== 	ENLACE HOJA DE ESTILOS (TERMINA)  =====================-->
+	<!--================== 	ENLACE HOJA DE ESTILOS <br>(TERMINA)  =====================-->
 
-	<!--================== 	ENLACE ICONO (INICIA)  =====================-->
+	<!--================== 	ENLACE ICONO <br>(INICIA)  =====================-->
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-	<!--================== 	ENLACE ICONO (TERMINA)  =====================-->
+	<!--================== 	ENLACE ICONO <br>(TERMINA)  =====================-->
 
-	<!--================== 	ENLACE GOOGLE ANALYTICS (INICIA)  =====================-->
+	<!--================== 	ENLACE GOOGLE ANALYTICS <br>(INICIA)  =====================-->
 
 	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  <br>(function<br>(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function<br>(){
+	  <br>(i[r].q=i[r].q||[]).push<br>(arguments)},i[r].l=1*new Date<br>();a=s.createElement<br>(o),
+	  m=s.getElementsByTagName<br>(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore<br>(a,m)
+	  })<br>(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-79840828-1', 'auto');
-	  ga('send', 'pageview');
+	  ga<br>('create', 'UA-79840828-1', 'auto');
+	  ga<br>('send', 'pageview');
 
 	</script>
 
-	<!--================== 	ENLACE GOOGLE ANALYTICS (TERMINA)  =====================-->
+	<!--================== 	ENLACE GOOGLE ANALYTICS <br>(TERMINA)  =====================-->
 
 </head>
 
@@ -64,7 +64,7 @@
 
 			Los productos de ХАDО pueden ser adquiridos en más de 30000 puntos certificados de venta. Actualmente se viene realizando un programa para creación de centros oficiales de servicio técnico “ХАDО-Service”, donde se realiza el diagnóstico y tratamiento garantizado con la aplicación de la tecnología Xado.<br><br>
 
-			Los productos ХАDО cuentan con licencia de API — American Petroleum Institute (Instituto americano del petróleo) en sus más altas especificaciones API SL y API SM, lo que demuestra la altísima calidad de los aceites para motor producidos.<br><br>
+			Los productos ХАDО cuentan con licencia de API — American Petroleum Institute <br>(Instituto americano del petróleo) en sus más altas especificaciones API SL y API SM, lo que demuestra la altísima calidad de los aceites para motor producidos.<br><br>
 
 			El descubrimiento del fenómeno “Revitalización” se convirtió en la esencia y eslabón clave de una nueva tecnología — La tecnología ХАDО. Los productos fabricados en base a esta tecnología poseen propiedades únicas, tanto restauradoras, protectoras como de reparación, lo que permite economizar en portadores de energía y recursos, proteger el medio ambiente creando un nueva calidad de vida.<br><br>
 
@@ -90,216 +90,215 @@
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_amc-maximum_250ml.jpg">
-					<h3><a href="xado-1stage-maximum.php">XADO® Atomic metal conditioner 1 Stage Maximum  for Diesel Truck (box, bottle 950 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
-
+					<h3><a href="xado-1stage-maximum.php">XADO® Atomic metal conditioner 1 Stage Maximum for Diesel Truck <br>(box, bottle 950 ml)</a></h3>
+					
 					<a class="mas-info-producto-xado" href="xado-1stage-maximum.php"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_amc-maximum-for-diesel-truck_950ml.jpg">
-					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum (box, bottle 225  ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum <br>(box, bottle 225  ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_amc-highway-disel-truck.jpg">
-					<h3><a href="">XADO® Atomic metal conditioner Highway for Diesel Truck (box, bottle 950 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® Atomic metal conditioner Highway for Diesel Truck <br>(box, bottle 950 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_amc-highway_225ml.jpg">
-					<h3><a href="">XADO® Atomic metal conditioner Highway (box, bottle 225 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® Atomic metal conditioner Highway <br>(box, bottle 225 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_1stage_magnum_box-maximum.jpg">
-					<h3><a href="">ХАDО® 1 STAGE engine revitalizant MAGNUM for diesel truck (blister package, tube 90 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">ХАDО® 1 STAGE engine revitalizant MAGNUM diesel truck <br>(blister package, tube 90 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_1stage_27ml.jpg">
-					<h3><a href="">ХАDО® 1 STAGE engine revitalizant (blister package, tube 27 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">ХАDО® 1 STAGE engine revitalizant <br>(blister package, tube 27 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_ex120-dlia_vseh_tipov_toplivnoy_apparatury_8ml.jpg">
-					<h3><a href="">ХАDО® REVITALIZANT EX120  for all types of fuel equipment (blister package, syringe 8 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">ХАDО® REVITALIZANT EX120  for all types of fuel equipment <br>(blister package, syringe 8 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_ex120-dlia_akpp_8ml.jpg">
-					<h3><a href="">ХАDО® REVITALIZANT EX120 for automatic transmission (blister package, syringe 8 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">ХАDО® REVITALIZANT EX120 for automatic transmission <br>(blister package, syringe 8 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_ex120-dlia_kpp_i_reduktorov_8ml.jpg">
-					<h3><a href="">XADO® REVITALIZANT EX120  for gear boxes, transfer  cases and differentials (blister package, syringe 8 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® REVITALIZANT EX120  for gear boxes, transfer cases and differentials <br>(blister package, syringe 8 ml)</a><br><br></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_injector_box-maximum-tr.jpg">
-					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum Transmission for Diesel Truck (syringe 50 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum Transmission for Diesel Truck <br>(syringe 50 ml)</a><br><br></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/amc-maximum-disel-truck-transmission-automat.jpg">
-					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum Automatic Transmission for Diesel Truck (syringe 50 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum Automatic Transmission for Diesel Truck <br>(syringe 50 ml)</a><br><br></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/amc-maximum-disel-truck-fuel_new.jpg">
-					<h3><a href="">XADO® Atomic metal conditioner with 1 Stage Revitalizant Maximum for Diesel Truck for all types of fuel equipment (syringe 30 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® Atomic metal conditioner with 1 Stage Revitalizant Maximum for Diesel Truck for all types of fuel equipment <br>(syringe 30 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">Antirain (aerosol can 320 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Antirain <br>(aerosol can 320 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">Antifog (aerosol can 320 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Antifog <br>(aerosol can 320 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado_gel-revitalizant-dlia-benzinovogo-dvigagatelia_9ml.jpg">
-					<h3><a href="">ХАDО® gel-REVITALIZANT for cylinders (blister package, tube 9 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">ХАDО® gel-REVITALIZANT for cylinders <br>(blister package, tube 9 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">Atomex Energy Drive. Power amplifier for gasoline engine (aluminum bottle 250 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Atomex Energy Drive. Power amplifier for gasoline engine <br>(aluminum bottle 250 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">Atomex Energy Drive. Power amplifier for diesel engine (aluminum bottle 250 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Atomex Energy Drive. Power amplifier for diesel engine <br>(aluminum bottle 250 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">ХАDО® protective grease (tube 125 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">ХАDО® protective grease <br>(tube 125 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">ХАDО® restoring grease (tube 125 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">ХАDО® restoring grease <br>(tube 125 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href=""> ХАDО® repairing grease (tube 125 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href=""> ХАDО® repairing grease <br>(tube 125 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/amc-maximum-disel-truck-transmission_new.jpg">
-					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum Transmission for Diesel Truck (syringe 50 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">XADO® Atomic metal conditioner 1 Stage Maximum Transmission for Diesel Truck <br>(syringe 50 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href=""> ХАDО® gel-REVITALIZANT SnipeX, for shooting sports equipment (blister package, tube 27  ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href=""> ХАDО® gel-REVITALIZANT SnipeX, for shooting sports equipment <br>(blister package, tube 27  ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">Atomex TotalFlush. Oil system cleaner (aluminum bottle 250 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Atomex TotalFlush. Oil system cleaner <br>(aluminum bottle 250 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">Atomex Multi Cleaner. High performance fuel system cleaner for gasoline and LPG engine (aluminum bottle 250 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Atomex Multi Cleaner. High performance fuel system cleaner for gasoline and LPG engine <br>(aluminum bottle 250 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/">
-					<h3><a href="">Atomex Multi Cleaner. High performance fuel system cleaner for diesel engine (aluminum bottle 250 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Atomex Multi Cleaner. High performance fuel system cleaner for diesel engine <br>(aluminum bottle 250 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado-xteme-cleaner.jpg">
-					<h3><a href="">Complex fuel system cleaner "Xtreme Cleaner for Diesel Truck" for up to 500 L of fuel (can 500 ml)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Complex fuel system cleaner "Xtreme Cleaner for Diesel Truck" for up to 500 L of fuel <br>(can 500 ml)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
 
 				<div class="producto-xado">
 					<img src="img/productos-xado/xado-xtreme-flush.jpg">
-					<h3><a href="">Oil System Cleaner «Xtreme Flush  for Diesel Truck» (can 0,5 l)</a></h3>
-					<p class="descripcion-producto-xado">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+					<h3><a href="">Oil System Cleaner «Xtreme Flush  for Diesel Truck» <br>(can 0,5 l)</a></h3>
+					
 
 					<a class="mas-info-producto-xado" href=""><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;MÁS INFO</a>
 				</div>
