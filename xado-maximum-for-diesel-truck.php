1caa1897bc02642acb46b9b8d37e0ae632fd809e
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-08-15 12:54:23 -0500

    Leer mas XADO

---
+++ xado-maximum-for-diesel-truck.php
@@ -0,0 +1,138 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<html lang="es">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width" />
+	<title>2MG - XADO MAXIMUM FOR DIESEL TRUCK</title>
+	<meta name="author" content="Brandemma" />
+	<meta name="description" content="2MG es una empresa peruana que se dedica a la importación y exportación de productos, servicios y soluciones en Procurement de Ingeniería."/>
+
+	<!--================== 	ENLACE HOJA DE ESTILOS (INICIA)  =====================-->
+
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+	<!--================== 	ENLACE HOJA DE ESTILOS (TERMINA)  =====================-->
+
+	<!--================== 	ENLACE ICONO (INICIA)  =====================-->
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+	<!--================== 	ENLACE ICONO (TERMINA)  =====================-->
+
+	<!--================== 	ENLACE GOOGLE ANALYTICS (INICIA)  =====================-->
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-79840828-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
+	<!--================== 	ENLACE GOOGLE ANALYTICS (TERMINA)  =====================-->
+
+</head>
+
+<body>
+	<header>
+		<?php
+			include 'menu.php';
+		?>
+	</header>
+	
+	<div class="contenedor-xado">
+		
+		<div class="descripcion-xado">
+			<div class="detalle-producto-xado-izq">
+				<img src="img/productos-xado/xado_amc-maximum_250ml.jpg">
+			</div>
+			
+			<div class="detalle-producto-xado-der">
+				<div class="titulo-producto-xado">
+					<h1>XADO Maximum for Diesel Truck</h1>
+				</div>
+
+				<div class="fondo-azul-xado">
+					<p>Acondicionador atómico para metal con revitalizante 1 Stage (RF 100).<br><br>
+
+					El compuesto está especialmente elaborado para la reparación restaudora y la protección activa de motores diesel de automóviles de cargas pesadas. Este producto innovador de 3 componentes combina ventajas del acondicionador complejo para metal de dos fases y del revitalizante 1 Stage.</p>
+				</div>
+
+				<h2>Tecnología y certificados</h2>
+				<div class="certificados-xado">
+					<img src="img/certificado-3c.jpg">
+					<img src="img/certificado-3rd.jpg">
+					<img src="img/certificado-62000.jpg">
+				</div>
+			</div>
+			<div class="clear"></div>
+
+
+			<h3>Ventajas</h3>
+			<ul>
+				<li><span>Restaura las superficies de las piezas y compensa el desgaste corriente.</span></li>
+				<li><span>Iguala y aumenta la compresión en los cilindros.</span></li>
+				<li><span>Aumenta la presión en el sistema de aceite hasta el valor nominal.</span></li>
+				<li><span>Crea un sistema activo de la protección de las piezas contra cargas y sobrecalentamiento.</span></li>
+				<li><span>Mejora propiedades lubricantes de aceite y características tribológicas de las unidades del motor.</span></li>
+				<li><span>Reduce el consumo de combustible.</span></li>
+				<li><span>Aumenta la potencia y capacidad de aceleración del motor.</span></li>
+				<li><span>Reduce ruidos y vibraciones.</span></li>
+				<li><span>Prolonga la vida útil del motor.</span></li>
+				<li><span>Asegura la protección contra el desgaste para 100.000 km de recorrido.</span></li>
+			</ul>
+
+			<div class="solicitud-nota-xado">
+				<div class="solicitud">
+					<h4>Aplicación</h4>
+					<p>
+						La temperatura del compuesto antes de la aplicación debe estar al menos + 25ºС. Agitar el frasco. Verter su contenido por el orificio de llenado de aceite del motor calentado previamente hasta la temperatura de funcionamiento. Arrancar el motor y dejarlo al ralentí durante 3-5 minutos.
+					</p>
+
+					<h4>Dosificación</h4>
+					<p>
+						1 frasco (950 ml) está destinado para el motor con el sistema de aceite de 30 – 45 l. El nivel de aceite no debe exceder la marca máxima. Evite la sobredosificación.
+					</p>
+
+					<h4>Embalaje</h4>
+					<p>
+						Frasco 950 ml Ref. XA 40712
+					</p>
+				</div>
+
+				<div class="nota">
+					<h4>Nota</h4>
+					<p>
+						El compuesto está especialmente elaborado y probado para motores diesel turbocargados de servicio pesado.<br><br>
+						El compuesto es compatible con todos los tipos de aceites para motores.<br><br>
+						La revitalización se realiza durante 1000-1500 km de recorrido; durante este tiempo no se recomienda cambiar el aceite.<br><br>
+						La aplicación repetida del producto XADO Maximum for Diesel Truck se recomienda después de 100 000 km de recorrido.<br><br>
+						Para asegurar el nivel superior de la protección del motor contra el desgaste se recomienda usar posteriormente aceites para motores XADO Atomic Oil.<br><br>
+						RF=100 RF (factor de revitalización) es un indicador de eficacia de la reparación y protección del motor contra el desgaste. RF=100 significa, que como resultado de la aplicación del producto, el motor ha pasado el ciclo completo de la revitalización y está totalmente restaurado y protegido con seguridad contra el desgaste para 100 000 km de recorrido.<br><br>
+						El producto contiene el revitalizante en gránulos, que se disuelve completamente en el aceite a la temperatura de funcionamiento.</p>
+				</div>
+			</div>
+
+			<div class="boton-solicitar-xado">
+				<a href="">Solicitalo &nbsp;<i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+			</div>
+
+		</div>
+
+		<?php
+			include 'contacto.php';
+		?>
+	</div>
+
+	<?php
+		include 'pie-pagina.php';
+	?>
+</body>
+
+</html>
